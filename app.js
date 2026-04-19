50c2b971840890288bf93f7722639e245e18f1d1
Shailendrayadav18 <{shailendrakyadav1872002@gmail.com}>
2026-04-20 03:02:33 +0530

    some fixes

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.set("trust proxy", 1);
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7*24*60*60*1000,
         maxAge: 7*24*60*60*1000,
