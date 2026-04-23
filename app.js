4cef35f67efaa1c10759d92e87b1cda8c4b556b3
Shailendrayadav18 <{shailendrakyadav1872002@gmail.com}>
2026-04-23 18:53:00 +0530

    issue fix 7

--- app.js
+++ app.js
@@ -60,7 +60,12 @@ app.use((err, req, res, next) => {
 });
 
 // ---------------- SERVER START ----------------
-const PORT = process.env.PORT || 8080;
+console.log("PORT VALUE:", process.env.PORT);
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  console.error("PORT not provided by Railway");
+}
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
